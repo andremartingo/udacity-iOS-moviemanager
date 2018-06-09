3522ba840e51e217b8fc4e4c66ece8001e00774d
andremartingo <andre.martingo@gmail.com>
2018-06-09 21:30:20 +0100

    create collectioViewController

---
+++ TheMovieManager/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  TheMovieManager
+//
+//  Created by André Martingo on 09/06/2018.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell{
+    
+    @IBOutlet weak var movieImageView: UIImageView!
+}
--- TheMovieManager/CollectionViewController.swift
+++ TheMovieManager/CollectionViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 // MARK: - WatchlistViewController: UIViewController
 
-class CollectionViewController: UIViewController {
+class CollectionViewController: UIViewController{
     
     // MARK: Properties
     
     var movies: [TMDBMovie] = [TMDBMovie]()
+    
+    fileprivate let itemsPerRow: CGFloat = 2
+    fileprivate let sectionInsets = UIEdgeInsets(top: 1.0, left: 1.0, bottom: 1.0, right: 1.0)
     
     // MARK: Outlets
     
@@ -27,6 +30,9 @@ class CollectionViewController: UIViewController {
         
         // create and set the logout button
         parent!.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .reply, target: self, action: #selector(logout))
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,7 +42,7 @@ class CollectionViewController: UIViewController {
             if let movies = movies {
                 self.movies = movies
                 performUIUpdatesOnMain {
-                    //self.moviesTableView.reloadData()
+                    self.collectionView.reloadData()
                 }
             } else {
                 print(error ?? "empty error")
@@ -49,8 +55,66 @@ class CollectionViewController: UIViewController {
     func logout() {
         dismiss(animated: true, completion: nil)
     }
+    
 }
 
-// MARK: - WatchlistViewController: UITableViewDelegate, UITableViewDataSource
+extension CollectionViewController: UICollectionViewDataSource,UICollectionViewDelegate {
+        
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return self.movies.count
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+            
+            let movie = self.movies[(indexPath as NSIndexPath).row]
+            
+            // Set the name and image
+            cell.movieImageView!.image = UIImage(named: "Film")
+            cell.movieImageView!.contentMode = UIViewContentMode.scaleAspectFit
+            
+            if let posterPath = movie.posterPath {
+                let _ = TMDBClient.sharedInstance().taskForGETImage(TMDBClient.PosterSizes.RowPoster, filePath: posterPath, completionHandlerForImage: { (imageData, error) in
+                    if let image = UIImage(data: imageData!) {
+                        performUIUpdatesOnMain {
+                            cell.movieImageView!.image = image
+                        }
+                    } else {
+                        print(error ?? "empty error")
+                    }
+                })
+            }
+            return cell
+        }
+}
 
+extension CollectionViewController : UICollectionViewDelegateFlowLayout {
+    
+    //1
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //2
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    //3
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    // 4
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+}
 
